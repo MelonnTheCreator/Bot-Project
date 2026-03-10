959bb7b419d192ab4c405684ed027e041ba7d715
MelonnTheCreator <grantkameron8@gmail.com>
2026-03-10 07:19:23 -0500

    update

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix=["!", "melonn ", "mel", "."], intents=intents)
+bot = commands.Bot(command_prefix=["m! ", "melonn ", "mel ", ".m "], intents=intents)
 
 @bot.event
 async def on_ready():
